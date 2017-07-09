2e6b7d0c75406e449e7c6bd33cceb75fddc48f68
Philip Damra <djheru@gmail.com>
2017-07-09 01:21:55 -0400

    tweak css

--- client/src/App.js
+++ client/src/App.js
@@ -1,55 +1,27 @@
 import React, { Component } from 'react';
-import { ApolloClient, gql, graphql, ApolloProvider, createNetworkInterface } from 'react-apollo';
-import { makeExecutableSchema } from 'graphql-tools';
-import { typeDefs } from './schema';
-import logo from './logo.svg';
 import './App.css';
-import AddChannel from './components/AddChannel';
+import ChannelsListWithData from './components/ChannelsListWithData';
 
-//const schema = makeExecutableSchema({ typeDefs });
-const networkInterface = createNetworkInterface({
-  uri: 'http://localhost:4000/graphql',
+import {
+  ApolloClient,
+  ApolloProvider,
+  createNetworkInterface,
+} from 'react-apollo';
+
+
+const networkInterface = createNetworkInterface({ uri: 'http://localhost:4000/graphql' });
+
+const client = new ApolloClient({
+  networkInterface,
 });
-const client = new ApolloClient({networkInterface});
-
-const channelsListQuery = gql`
-  query ChannelsListQuery {
-    channels {
-      id
-      name
-    }
-  }
-`;
-
-const ChannelsList = ({ data: { loading, error, channels } }) => {
-  if (loading) {
-    return (<p>Loading ...</p>);
-  }
-  if (error) {
-    return (<p>{error.message}</p>);
-  }
-  return (
-    <ul className="Item-list">
-      { channels.map(ch => (<li key={ch.id}>{ch.name}</li>)) }
-    </ul>
-  )
-};
-
-const ChannelsListWithData = graphql(channelsListQuery)(ChannelsList);
 
 class App extends Component {
   render() {
     return (
       <ApolloProvider client={client}>
         <div className="App">
-          <div className="App-header">
-            <img src={logo} className="App-logo" alt="logo"/>
-            <h2>Welcome to Apollo</h2>
-          </div>
-          <div className="Channels-container">
-            <AddChannel/>
-            <ChannelsListWithData/>
-          </div>
+          <div className="navbar">React + GraphQL Example</div>
+          <ChannelsListWithData />
         </div>
       </ApolloProvider>
     );
